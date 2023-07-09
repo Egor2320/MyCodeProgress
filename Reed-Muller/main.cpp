a2db63d9fb55e25ff4f97286d8595c46bea8eb94
Egor2320 <128361560+Egor2320@users.noreply.github.com>
2023-07-09 16:07:40 +0300

    Update main.cpp

--- Reed-Muller/main.cpp
+++ Reed-Muller/main.cpp
@@ -1,12 +1,11 @@
-#include <iostream>
 #include "popcounter.h"
 #include "logical_function.h"
 
 int main() {
     Reed_Muller::popcounter p;
     std::vector<int> v = p(52);
-    for(int i = 0; i < v.size(); ++i){
-        std::cout << v[i] << ' ';
-    }
+
+    Reed_Muller::logical_function f(3, "10100111");
+    f.print();
     return 0;
 }
