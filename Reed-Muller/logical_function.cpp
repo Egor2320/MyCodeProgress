60bb767eb2181be1115a38bbf6354e5abccd5f0b
Egor2320 <128361560+Egor2320@users.noreply.github.com>
2023-07-10 11:43:21 +0300

    Update logical_function.cpp

--- Reed-Muller/logical_function.cpp
+++ Reed-Muller/logical_function.cpp
@@ -7,14 +7,14 @@ namespace Reed_Muller{
     logical_function::logical_function(int n, std::string truth_table) {
         popcounter p;
 
+        std::vector<cell> func_tmp;
         num = n;
-        func.resize(pow(2, n));
-        int len = truth_table.size();
-        func[0].coef = truth_table[0] - '0';
-
+        func_tmp.resize(pow(2, n));
+        size_t len = truth_table.size();
+        func_tmp[0].coef = truth_table[0] - '0';
         for (int i = 1; i < len; ++i){
             std::vector<int> tmp = p(i);
-            func[i].variables = tmp;
+            func_tmp[i].variables = tmp;
             int cur_num_of_var = tmp.size();
             int cur_coef = (truth_table[i] - '0');
             for(int k = 0; k < pow(2, cur_num_of_var); ++k){
@@ -27,38 +27,30 @@ namespace Reed_Muller{
                     }
                 }
 
-                cur_coef ^= func[pos].coef;
+                cur_coef ^= func_tmp[pos].coef;
             }
-            func[i].coef = cur_coef;
+            func_tmp[i].coef = cur_coef;
 
         }
+
+        for(const cell& c : func_tmp){
+            if(c.coef == 1) func.push_back(c);
+        }
+
 
     }
 
     logical_function::~logical_function() = default;
 
-    void logical_function::print() {
-        for (int i = 0; i < func.size(); ++i){
-            std::cout << func[i]. coef << '\n';
-            if(!i) std::cout << "no args ";
-            else std::cout << "args: ";
-            for (int j = 0; j < func[i].variables.size(); ++j){
-                std::cout << func[i].variables[j] + 1 << ' ';
-            }
-            std::cout << '\n';
-        }
-    }
-
     int logical_function::operator()(std::vector<int> v) {
         std::reverse(v.begin(), v.end());
         int res = 0;
-        for(int i = 0; i < func.size(); ++i){
-            if(func[i].coef == 0) continue;
+        for(auto & i : func){
             int carry = 1;
-            for(int j = 0; j < func[i].variables.size(); ++j){
-                carry &= v[func[i].variables[j]];
+            for(int variable : i.variables){
+                carry &= v[variable];
             }
-            carry &= func[i].coef;
+            carry &= i.coef;
             res ^= carry;
         }
         return res;
@@ -66,8 +58,8 @@ namespace Reed_Muller{
 
     int logical_function::operator()(std::string s) {
         std::vector<int> v;
-        int len = s.size();
-        for(int i = 0; i < len; ++i){
+        size_t len = s.size();
+        for(size_t i = 0; i < len; ++i){
             v.push_back(s[i] - '0');
         }
         return logical_function::operator()(v);
@@ -75,18 +67,14 @@ namespace Reed_Muller{
 
     void logical_function::polynomical() {
         for(int i = 0; i < func.size(); ++i){
-            if(func[i].coef == 0) continue;
-            else{
-                if(i == 0){
-                    std::cout << "1 ";
-
-
-                }
-                for(int j = 0; j < func[i].variables.size(); ++j){
-                    std::cout << "x_" << func[i].variables[j] + 1 << ' ';
-                }
-               if(i != func.size() - 1)std::cout << "^ ";
+            if(i == 0){
+                std::cout << "1 ";
             }
+            for(int variable : func[i].variables){
+                std::cout << "x_" << variable + 1 << ' ';
+            }
+            if(i != func.size() - 1)std::cout << "^ ";
+
         }
     }
 
