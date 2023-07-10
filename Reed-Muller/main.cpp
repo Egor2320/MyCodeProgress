78294b2b2e77d5b61b33530cd1092352174aaf55
Egor2320 <128361560+Egor2320@users.noreply.github.com>
2023-07-10 11:48:41 +0300

    Update main.cpp

--- Reed-Muller/main.cpp
+++ Reed-Muller/main.cpp
@@ -2,18 +2,17 @@
 #include "logical_function.h"
 
 int main() {
-    Reed_Muller::popcounter p;
+  Reed_Muller::popcounter p;
 
-    Reed_Muller::logical_function f(3, "10110001");
+  Reed_Muller::logical_function f(3, "10110001");
 
-    for(int i = 0; i < 8; ++i){
-        std::string s = p.to2(i, 3);
-        std::cout << s << "->" << f(s) << '\n';
-    }
+  for(int i = 0; i < 8; ++i){
+  std::string s = p.to2(i, 3);
+  std::cout << s << "->" << f(s) << '\n';
+  }
+
+		f.polynomical();
 
 
-    f.polynomical();
-
-
-    return 0;
+  return 0;
 }
